806e2794999373bfad1de1eaab7701817f9fcff3
Takuya <takuya@disroot.org>
2018-12-07 17:45:25 +0100

    First exercise completed

--- AreaTriangulo.cpp
+++ AreaTriangulo.cpp
@@ -2,6 +2,17 @@
 using namespace std;
 
 int main(){
-    cout<<"Fichero independiente"<<endl;
+    //1.- Escriba un programa que nos permita hallar el área de un triángulo
+    float base, altura, area;
+    
+    cout << "Ingrese la base del triángulo: ";
+    cin >> base;
+    
+    cout << "Ingrese la altura del triángulo: ";
+    cin >> altura;
+    
+    area = base * altura / 2;
+    cout << "El área del triángulo es: " << area << endl;
+    
     return 0;
 }
