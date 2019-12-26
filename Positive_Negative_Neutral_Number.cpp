c8f2c5254c4d354d6e5105fc751f746613a07ad2
Takuya <takuya@disroot.org>
2019-12-26 14:54:48 +0100

    Positive_Negative_Neutral_Number exercise completed.

---
+++ Positive_Negative_Neutral_Number.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int	main()
+{
+	/**Escriba un programa que diga si un número
+	  entero es positivo, negativo o neutro.**/
+	int number;
+
+	cout << "Input the number: ";
+	cin >> number;
+	if (number > 0)
+	{
+		cout << "The number is positive" << endl;
+	}
+	else
+	{
+		if (number < 0)
+			cout << "The number is negative" << endl;
+		else
+		{
+			cout << "The number is neutral" << endl;
+		}
+	}
+}
